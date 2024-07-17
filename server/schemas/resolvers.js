2ee3292fc89391c0ad15dc34d45139bb272b51c6
kaylommy <kaylajfreeman13@gmail.com>
2024-07-17 11:57:14 -0400

    added queries and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { Query } = require('../../../fitsync-pro/server/schemas/resolvers');
+const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
@@ -8,21 +8,50 @@ const resolvers = {
             if  (context.user) {
                 return await User.findOne({ _id: context.user._id });
             }
-            throw new Error('You must be logged in!');
+            throw new AuthenticationError('You must be logged in!');
         },
     },
     Mutation: {
         login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user){
+                throw new AuthenticationError('Incorrect Email');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw){
+                throw new AuthenticationError('Incorrect password');
+            }
+
+            const token = signToken(user);
+            return { token, user };
 
         },
         addUser: async (parent, { username, email, password }) => {
-
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user }
         },
         saveBook: async (parent, book, context) => {
-
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: book }},
+                    { new: true }
+                )
+            }
+            throw new AuthenticationError('You must be logged in!');
         },
         removeBook: async (parent, { bookId }, context) => {
-
+            if (context.user){
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } }},
+                    { new: true }
+                )
+            }
+            throw new AuthenticationError('You must be logged in!')
         }
     }
 }
