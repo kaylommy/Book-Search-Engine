eea503d5acfc0890fbc355492352dbbff04405c1
kaylommy <kaylajfreeman13@gmail.com>
2024-07-16 20:04:51 -0400

    updated authMiddleware and implemented Apollo server and applied it to Express server

--- server/server.js
+++ server/server.js
@@ -3,11 +3,26 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Apollo Server and Middleware
+const { ApolloServer } = require('@apollo/server');
+const { authMiddleware } = require('./utils/auth'); 
+const { typeDefs, resolvers } = require('./schemas')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const startApolloServer = async () => {
+await server.start();
+server.applyMiddleware({ app })
+};
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -19,3 +34,5 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startApolloServer();
